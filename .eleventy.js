52390330cbb48e1c951f00324299eb317401a958
Rob Hudson <rob@cogit8.org>
2020-10-25 11:18:33 -0700

    Remove eleventy-navigation

--- .eleventy.js
+++ .eleventy.js
@@ -2,14 +2,12 @@ const { DateTime } = require("luxon");
 const fs = require("fs");
 const pluginRss = require("@11ty/eleventy-plugin-rss");
 const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
-const pluginNavigation = require("@11ty/eleventy-navigation");
 const markdownIt = require("markdown-it");
 const markdownItAnchor = require("markdown-it-anchor");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(pluginRss);
   eleventyConfig.addPlugin(pluginSyntaxHighlight);
-  eleventyConfig.addPlugin(pluginNavigation);
 
   eleventyConfig.setDataDeepMerge(true);
 
@@ -19,7 +17,6 @@ module.exports = function (eleventyConfig) {
     return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat("DDD");
   });
 
-  // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
   eleventyConfig.addFilter("dateISO", (dateObj) => {
     return DateTime.fromJSDate(dateObj, { zone: "utc" }).toISO();
   });
@@ -80,7 +77,6 @@ module.exports = function (eleventyConfig) {
     linkify: true,
   }).use(markdownItAnchor, {
     permalink: true,
-    permalinkClass: "direct-link",
     permalinkSymbol: "#",
   });
   eleventyConfig.setLibrary("md", markdownLibrary);
@@ -105,21 +101,10 @@ module.exports = function (eleventyConfig) {
   return {
     templateFormats: ["md", "njk", "html", "liquid"],
 
-    // If your site lives in a different subdirectory, change this.
-    // Leading or trailing slashes are all normalized away, so don’t worry about those.
-
-    // If you don’t have a subdirectory, use "" or "/" (they do the same thing)
-    // This is only used for link URLs (it does not affect your file structure)
-    // Best paired with the `url` filter: https://www.11ty.dev/docs/filters/url/
-
-    // You can also pass this in on the command line using `--pathprefix`
-    // pathPrefix: "/",
-
     markdownTemplateEngine: "liquid",
     htmlTemplateEngine: "njk",
     dataTemplateEngine: "njk",
 
-    // These are all optional, defaults are shown:
     dir: {
       input: "src",
       includes: "_includes",
--- src/about/index.md
+++ src/about/index.md
@@ -3,9 +3,6 @@ layout: layouts/home.njk
 title: About Me
 templateClass: tmpl-post
 bgColor: bg-blue-800
-eleventyNavigation:
-  key: About
-  order: 3
 ---
 
 I am a person that writes stuff.
