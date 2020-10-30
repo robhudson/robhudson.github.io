38ab5a7668a3075472dd6a109f774b1e3043e0ae
Rob Hudson <rob@cogit8.org>
2020-10-30 15:40:12 -0700

    Purge markdown tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,42 @@
+const markdownIt = require("markdown-it");
+
+const purgeMarkdown = (content) => {
+  const md = markdownIt().render(content);
+
+  // Capture as liberally as possible, including things like `h-(screen-1.5)`
+  const broadMatches = md.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || [];
+
+  // Capture classes within other delimiters like .block(class="w-1/2") in Pug
+  const innerMatches =
+    md.match(/[^<>"'`\s.(){}[\]#=%]*[^<>"'`\s.(){}[\]#=%:]/g) || [];
+
+  return broadMatches.concat(innerMatches);
+};
+
 module.exports = {
   future: {
     // removeDeprecatedGapUtilities: true,
     purgeLayersByDefault: true,
   },
   purge: {
-    content: ["./src/**/*.njk"],
+    mode: "all",
+    content: ["./src/**/*.{njk,md}"],
     options: {
-      defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-      whitelist: ['bg-green-900', 'bg-teal-800', 'bg-blue-800']
+      extractors: [
+        {
+          extractor: purgeMarkdown,
+          extensions: ["md"],
+        },
+      ],
     },
   },
   theme: {
     extend: {
       fontFamily: {
-        'heading': ['Fira Sans', 'sans-serif'],
-      }
+        heading: ["Fira Sans", "sans-serif"],
+      },
     },
   },
   variants: {},
-  plugins: [
-    require('@tailwindcss/typography'),
-  ],
+  plugins: [require("@tailwindcss/typography")],
 };
